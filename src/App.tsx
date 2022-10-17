179ca91cf545de968b2f3747a3d9aaa4ab748197
Luca Ziliani <lucagcc@gmail.com>
2022-10-17 08:50:26 +0200

    git hub action test

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <News />
         </Section>
         <Banner
-          title={`we are. 2`}
+          title={`we are. 3`}
           description={`Consectetur adipiscing elit.
         Nulla condimentum tortor sem.`}
           buttonText="THE PLAYERS >"
